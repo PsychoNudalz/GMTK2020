1ad16e16225e3245ad06e8764adf605a4e37c36e
Anson <anson.aw0618@gmail.com>
2020-07-11 00:39:38 +0100

    Anson: Adding UI

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour
 {
+    [Header("Time Loader")]
     [SerializeField] private float startTime;
     [SerializeField] private float currentTime;
+    public TextMeshProUGUI TimeUI;
+
+    [Header("Player States")]
+    public PlayerState playerState;
+    public TextMeshPro SpeedUI;
 
     // Start is called before the first frame update
     void Start()
@@ -17,5 +24,12 @@ public class GameManagerScript : MonoBehaviour
     void Update()
     {
         currentTime = Time.time - startTime;
+        updateTime();
+        
+    }
+
+    void updateTime()
+    {
+        TimeUI.text = currentTime.ToString("0");
     }
 }
