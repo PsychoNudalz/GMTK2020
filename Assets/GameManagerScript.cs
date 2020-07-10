13702e9a3d606dd7c74ec593e80289145bc1d668
Anson <anson.aw0618@gmail.com>
2020-07-10 23:47:07 +0100

    Anson: remove meta file

---
+++ Assets/GameManagerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    [SerializeField] private float startTime;
+    [SerializeField] private float currentTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime = Time.time - startTime;
+    }
+}
