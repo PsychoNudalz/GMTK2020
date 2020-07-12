ca2ff69ca2286fc3566f66ba42c6e9e614a1cae4
Daniel <Newsom>
2020-07-12 15:33:48 +0100

    Danny: Add Game Music

---
+++ Assets/MenuMusicScript.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMusicScript : MonoBehaviour
+{
+
+    public static AudioSource menuMusic;
+    
+    
+        private static MenuMusicScript _instance;
+        
+        public static MenuMusicScript instance
+        {
+
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = GameObject.FindObjectOfType<MenuMusicScript>();
+
+                    //Tell unity not to destroy this object when loading a new scene!
+                    DontDestroyOnLoad(_instance.gameObject);
+                menuMusic = instance.gameObject.GetComponent<AudioSource>();
+                }
+
+                return _instance;
+            }
+        }
+
+        void Awake()
+        {
+            if (_instance == null)
+            {
+                //If I am the first instance, make me the Singleton
+                _instance = this;
+                DontDestroyOnLoad(this);
+            }
+            else
+            {
+                //If a Singleton already exists and you find
+                //another reference in scene, destroy it!
+                if (this != _instance)
+                    Destroy(this.gameObject);
+            }
+        }
+
+    
+
+    /*
+    
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        menuMusic = GetComponent<AudioSource>();
+    }
+    */
+    public void PlayMusic()
+    {
+        if (menuMusic.isPlaying) return;
+        menuMusic.Play();
+    }
+
+    public void StopMusic()
+    {
+        menuMusic.Stop();
+    }
+    
+}
+
---
+++ Assets/MenuMusicStop.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMusicStop : MonoBehaviour
+{
+    private MenuMusicScript _instance;
+    private void Awake()
+    {
+        _instance = GameObject.FindObjectOfType<MenuMusicScript>();
+        _instance.StopMusic();
+    }
+    
+    
+
+}
