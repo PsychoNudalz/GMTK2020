28581d34d6a2203367c783f80bcf7a2629f3676d
Daniel <Newsom>
2020-07-12 16:25:54 +0100

    Auto stash before merge of "Danny" and "origin/Danny"

---
+++ Assets/Scripts/ControlHide.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlHide: MonoBehaviour
+{
+    public GameObject ControlsUI;
+    public GameObject icon;
+    public AudioSource pickup;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            ControlsUI = GameObject.FindGameObjectWithTag("ControlUI");
+            StartCoroutine(HideControl());
+            pickup.Play();
+            icon.SetActive(false);
+        }
+    }
+
+    IEnumerator HideControl()
+    {
+        ControlsUI.SetActive(false);
+        yield return new WaitForSeconds(5);
+        ControlsUI.SetActive(true);
+        Destroy(gameObject);
+
+    }
+}
